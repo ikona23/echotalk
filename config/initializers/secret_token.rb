e5b67e1c6f56b6056beae2c88a6b7910fb9ce92e
ikona23@gmail.com <ikona23@gmail.com>
2014-10-27 03:03:37 +1300

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echotalk::Application.config.secret_token = '9f64d7be4b085f33a16420d4d2bd0315cdcb6a680dc9691577a64bc2afd4a21dc4f13c5abc9b8bde9b96f474cded854e1392fb37e62dd4a5736e2435e02aee58'
